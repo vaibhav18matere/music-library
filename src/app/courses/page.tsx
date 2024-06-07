3cbfed0b184c645cc9683affbfdc28f65de03242
vaibhav18matere <vpm00718897@gmail.com>
2024-06-07 16:11:38 +0530

    bcg gradient added

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,7 +1,7 @@
 export default function Courses() {
-     return (
-          <>
-          <h1 className="text-2xl text-center mt-6">Explore all our courses</h1>
-          </>
-     )
+  return (
+    <>
+      <h1 className="text-2xl text-center mt-6">Explore all our courses</h1>
+    </>
+  );
 }
