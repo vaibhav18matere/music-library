9a226defdab4f2b226db97905a5dce136bd70b37
vaibhav18matere <vpm00718897@gmail.com>
2024-06-07 11:29:35 +0530

    hero section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from "./components/Navbar";
+import Hero from "./components/ui/Hero";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialised bg-grid-white/[0.2]">
       <Navbar />
+      <Hero />
     </main>
   );
 }
